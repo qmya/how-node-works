b2cf33fb2c359fc7f25f78e735e701c6de00668f
qmya <qandeelgul90@gmail.com>
2021-01-13 15:13:56 -0500

    the encrytion and threadpool practice

--- event-loop.js
+++ event-loop.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
-const 
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 3;
+
+console.log(start);
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
 
@@ -11,6 +16,19 @@ fs.readFile("text-file.txt", () => {
   setImmediate(() => console.log("Immediate 2 finished"));
 
   process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password Encrypted");
+  });
 });
 
 console.log("Hello from the top-level code");
