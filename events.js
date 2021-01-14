917f54a008ffdbe462821b40bc6d6da125504829
qmya <qandeelgul90@gmail.com>
2021-01-14 10:33:53 -0500

    created server and add close and start server event emitter to it

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
 const EventEmitter = require("events"); //to use events module we need to require event emitter class
-
+const http = require("http");
 class Sales extends EventEmitter {
   constructor() {
     super();
@@ -26,3 +26,29 @@ myEmitter.on("newSale", (stock) => {
 //this is object that emits the emitters
 myEmitter.emit("newSale", 9); //emit is just like we are clicking on a button
 ////////////////////////////////////////
+//const server = createServer();
+const server = http.createServer(); //another way of creating server
+//if you see an "on" in the node project it means you are listening to the event
+server.on("request", (req, res) => {
+  console.log("Request recieved");
+  console.log(req.url);
+  res.end("Request recieved");
+});
+
+//We can listener multiple times to the event
+server.on("request", (req, res) => {
+  console.log("Another Request 😀");
+});
+
+//We can also listen to the close event
+//this event is fired when the server is closed
+server.on("close", () => {
+  console.log("Server is close");
+});
+
+//We can also listen to the START the server
+//this event is fired when the server is STARTED
+//127.0.0.1 = localhost
+server.listen(8000, "127.0.0.1", () => {
+  console.log("The server is running on localhost port 8000");
+});
