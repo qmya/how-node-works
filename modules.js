5fcac98ef2113bf4eb9ff6a87bf2838f97d1b518
qmya <qandeelgul90@gmail.com>
2021-01-14 15:04:40 -0500

    exporting the module from one file to another by using short hand export objects

--- modules.js
+++ modules.js
@@ -8,4 +8,10 @@ const calc1 = new C();
 
 console.log(calc1.add(2, 5));
 
-//exports
+//exports(short hands)
+const calc2 = require("./test-module-2");
+const { add, multiply, divide } = require("./test-module-2");
+console.log(calc2.add(2, 5));
+console.log(calc2.multiply(2, 5));
+
+//cachings
